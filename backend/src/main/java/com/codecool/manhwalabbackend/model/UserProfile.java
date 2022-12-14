694c3afced712c45f0fb993f2019f19804bda1ff
AronBodzas <aron.bodzas42@gmail.com>
2022-12-14 12:58:52 +0100

    Feat: user entity

---
+++ backend/src/main/java/com/codecool/manhwalabbackend/model/UserProfile.java
@@ -0,0 +1,33 @@
+package com.codecool.manhwalabbackend.model;
+
+import com.codecool.manhwalabbackend.model.Enums.Gender;
+import javax.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ProfileID")
+    @SequenceGenerator(name = "ProfileID", sequenceName = "ProfileID", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String userName;
+    private String description;
+    private Integer age;
+    private String Password;
+    private Gender gender;
+    private String location;
+    private Timestamp lastOnline;
+    private Timestamp joined;
+//    profile picture
+
+}
+
