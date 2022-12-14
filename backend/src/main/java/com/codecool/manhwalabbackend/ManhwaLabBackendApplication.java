bec2755c4311e8c3faae09edf39be5f9a61a9037
AronBodzas <aron.bodzas42@gmail.com>
2022-12-14 09:36:31 +0100

    chore: initialising project backend

---
+++ backend/src/main/java/com/codecool/manhwalabbackend/ManhwaLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.manhwalabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManhwaLabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManhwaLabBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/codecool/manhwalabbackend/ManhwaLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.manhwalabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManhwaLabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
