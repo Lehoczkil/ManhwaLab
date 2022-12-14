1b590bce40f2b74d34e2d4c7ba23d927d4d14ca6
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-14 11:43:24 +0100

    feat: create header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <div class="search-container">
+            <input type="search" placeholder="Find manhwa...">
+            <select class="menu">
+                <option value="">Browse</option>
+                <option value="">Recommendations</option>
+            </select>
+        </div>
+        <img src="../assets/logo.png" alt="logo">
+        <div class="auth">
+            <button>Register</button>
+            <button>Login</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
