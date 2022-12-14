19bf31a1999dbef5c6379813625bec5522d5223e
AronBodzas <aron.bodzas42@gmail.com>
2022-12-14 13:28:43 +0100

    Feat: genre repository injected

--- backend/src/main/java/com/codecool/manhwalabbackend/service/GenreService.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/GenreService.java
@@ -1,7 +1,15 @@
 package com.codecool.manhwalabbackend.service;
 
+import com.codecool.manhwalabbackend.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GenreService {
+    GenreRepository genreRepository;
+
+    @Autowired
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
 }
