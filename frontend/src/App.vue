ee64f534650054afba9074cadb4167f42fc6aafa
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-14 10:16:06 +0100

    chore: initialize frontend with vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+button:active {
+    transform: scale(0.95);
+}
+
+.container {
+  min-height: 100vh;
+}
+</style>
