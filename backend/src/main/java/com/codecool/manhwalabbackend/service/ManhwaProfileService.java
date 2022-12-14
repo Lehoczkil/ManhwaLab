3a522343eb26b96826c344b4132f1ac7a1694ce2
AronBodzas <aron.bodzas42@gmail.com>
2022-12-14 13:28:56 +0100

    Feat: manhwaProfile repository injected

--- backend/src/main/java/com/codecool/manhwalabbackend/service/ManhwaProfileService.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/ManhwaProfileService.java
@@ -1,9 +1,17 @@
 package com.codecool.manhwalabbackend.service;
 
+import com.codecool.manhwalabbackend.repository.ManhwaProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ManhwaProfileService {
 
+    ManhwaProfileRepository manhwaProfileRepository;
+
+    @Autowired
+    public ManhwaProfileService(ManhwaProfileRepository manhwaProfileRepository) {
+        this.manhwaProfileRepository = manhwaProfileRepository;
+    }
 }
 
