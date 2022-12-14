30ec951b5cf56bc3b7ce29b898f5888fc0c215a8
AronBodzas <aron.bodzas42@gmail.com>
2022-12-14 13:29:18 +0100

    Feat: userProfile repository injected

--- backend/src/main/java/com/codecool/manhwalabbackend/service/UserProfileService.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/UserProfileService.java
@@ -1,9 +1,17 @@
 package com.codecool.manhwalabbackend.service;
 
+import com.codecool.manhwalabbackend.repository.UserProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserProfileService {
 
+    UserProfileRepository userProfileRepository;
+
+    @Autowired
+    public UserProfileService(UserProfileRepository userProfileRepository) {
+        this.userProfileRepository = userProfileRepository;
+    }
 }
 
