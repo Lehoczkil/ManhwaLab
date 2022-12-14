9df46cac5784fba87c95c49f08462c20fab6ccbe
AronBodzas <aron.bodzas42@gmail.com>
2022-12-14 13:23:29 +0100

    Feat: making services for entities

---
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/GenreService.java
@@ -0,0 +1,7 @@
+package com.codecool.manhwalabbackend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenreService {
+}
---
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/ManhwaProfileService.java
@@ -0,0 +1,9 @@
+package com.codecool.manhwalabbackend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManhwaProfileService {
+
+}
+
