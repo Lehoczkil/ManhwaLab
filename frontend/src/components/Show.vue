64a613e3ccaf420a0d243aeb8681bbed7b6d01db
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-15 10:25:11 +0100

    feat: add basic show component for slider

---
+++ frontend/src/components/Show.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="show">
+        <div class="show-pic"></div>
+        <h1>Title</h1>
+        <p>Description</p>
+        <div class="rating">
+            <img class="star" src="../assets/left.png" alt="left-arrow">
+            <img class="star" src="../assets/left.png" alt="left-arrow">
+            <img class="star" src="../assets/left.png" alt="left-arrow">
+            <img class="star" src="../assets/left.png" alt="left-arrow">
+            <img class="star" src="../assets/left.png" alt="left-arrow">
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.star {
+    height: 2vh;
+}
+
+.show-pic {
+    height: clamp(100px, 12vh, 700px);
+    width: clamp(72px, 8.5vh, 350px);
+    background: black;
+}
+</style>
+
+<script>
+export default {
+    name: 'Show'
+}
+</script>
