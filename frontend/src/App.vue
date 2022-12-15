dedebeef963f64f710ac5b281e4155ca1c6c3948
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-15 09:02:45 +0100

    feat: create basic footer component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,15 +2,19 @@
   <div class="container">
     <Header />
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
+
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
