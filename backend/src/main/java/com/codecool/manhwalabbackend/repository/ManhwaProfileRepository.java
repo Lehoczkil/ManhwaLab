2978744b8df4bf5ede992732305b55ed167c8ba3
AronBodzas <aron.bodzas42@gmail.com>
2022-12-15 11:15:50 +0100

    Feat: repository interfaces with jpaRepository

--- backend/src/main/java/com/codecool/manhwalabbackend/repository/ManhwaProfileRepository.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/repository/ManhwaProfileRepository.java
@@ -1,5 +1,7 @@
 package com.codecool.manhwalabbackend.repository;
 
+import com.codecool.manhwalabbackend.model.ManhwaProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- backend/src/main/java/com/codecool/manhwalabbackend/repository/ThemeRepository.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/repository/ThemeRepository.java
@@ -1,9 +1,11 @@
 package com.codecool.manhwalabbackend.repository;
 
+import com.codecool.manhwalabbackend.model.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ThemeRepository {
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
 
 }
 
