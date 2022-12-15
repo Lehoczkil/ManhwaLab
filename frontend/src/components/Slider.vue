8bee64fd4024830bb73346f6ad443cc51e25d219
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-15 10:25:55 +0100

    feat: add basic slider component for browsing shows

--- frontend/src/components/Slider.vue
+++ frontend/src/components/Slider.vue
@@ -1,12 +1,51 @@
 <template>
     <section>
         <h1>Test</h1>
-        <div class="show-container"></div>
+        <div class="show-container">
+            <img class="arrow" src="../assets/left.png" alt="left-arrow">
+            <div class="shows">
+                <Show />
+                <Show />
+                <Show />
+                <Show />
+                <Show />
+                <Show />
+                <Show />
+            </div>
+            <img class="arrow" src="../assets/right.png" alt="right-arrow">
+        </div>
     </section>
 </template>
 
+<style scoped>
+.show-container, .shows {
+    display: flex;
+    justify-content: space-between;
+    background: red;
+    height: clamp(200px, 15vh, 1200px);
+    align-items: center;
+}
+
+.shows {
+    background: purple;
+    width: 50vw;
+}
+
+.arrow {
+    height: 8vh;
+}
+</style>
+
 <script>
+import Show from './Show'
+
 export default {
-    name: 'Slider'
+    name: 'Slider',
+    components: {
+        Show
+    }
 }
 </script>
+
+
+
