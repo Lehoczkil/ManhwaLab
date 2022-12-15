dfb135d2809f6d222115072b69e67aa8f95ffd48
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-15 11:32:26 +0100

    feat: add new route for viewing a mangas profile

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home";
+import MangaProfile from "../views/MangaProfile";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },{
+    path: "/manga-profile",
+    name: "MangaProfile",
+    component: MangaProfile,
   },
 ];
 
