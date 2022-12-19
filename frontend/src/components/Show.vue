d896ddc2777d3137a80af5bc5c9b1c30aad728b5
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-19 10:09:01 +0100

    style: adjust size of show pic

--- frontend/src/components/Show.vue
+++ frontend/src/components/Show.vue
@@ -2,13 +2,13 @@
     <div class="show">
         <div class="show-pic"></div>
         <h1>Title</h1>
-        <p>Description</p>
+        <p>112 ch</p>
         <div class="rating">
-            <img class="star" src="../assets/left.png" alt="left-arrow">
-            <img class="star" src="../assets/left.png" alt="left-arrow">
-            <img class="star" src="../assets/left.png" alt="left-arrow">
-            <img class="star" src="../assets/left.png" alt="left-arrow">
-            <img class="star" src="../assets/left.png" alt="left-arrow">
+            <img class="star" src="../assets/star.png" alt="star">
+            <img class="star" src="../assets/star.png" alt="star">
+            <img class="star" src="../assets/star.png" alt="star">
+            <img class="star" src="../assets/star.png" alt="star">
+            <img class="star" src="../assets/star.png" alt="star">
         </div>
     </div>
 </template>
@@ -20,8 +20,8 @@
 }
 
 .show-pic {
-    height: clamp(100px, 12vh, 700px);
-    width: clamp(72px, 8.5vh, 350px);
+    height: clamp(100px, 12.5vh, 700px);
+    width: clamp(72px, 9vh, 350px);
     background: black;
 }
 </style>
