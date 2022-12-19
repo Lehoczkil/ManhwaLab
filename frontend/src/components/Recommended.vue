a161aff2c3f9afb131fb4c341ab53c2fd345d696
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-19 10:11:11 +0100

    feat: add component for giving recommendtaions to users in manga profile

---
+++ frontend/src/components/Recommended.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="recommended">
+        <h1>More like this</h1>
+        <div class="shows">
+            <Show />
+            <Show />
+            <Show />
+            <Show />
+            <Show />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    .recommended {
+        background: purple;
+        margin-inline: 2vw;
+        width: 50vw;
+        margin: auto;
+    }
+    .shows {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    h1 {
+        margin-left: 2.5vw;
+    }
+</style>
+
+<script>
+import Show from './Show'
+export default {
+    name: 'Recommended',
+    components: {
+        Show
+    }
+}
+</script>
