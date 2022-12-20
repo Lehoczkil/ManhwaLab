bffdcfa0206c77670c9b9b8b26e8c334d1d22850
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-12-20 10:10:46 +0100

    feat: add new route for comics page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home";
 import MangaProfile from "../views/MangaProfile";
 import UserProfile from "../views/UserProfile";
+import Comics from "../views/Comics";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/user-profile",
     name: "UserProfile",
     component: UserProfile,
+  },
+  {
+    path: "/comics",
+    name: "Comics",
+    component: Comics,
   },
 ];
 
