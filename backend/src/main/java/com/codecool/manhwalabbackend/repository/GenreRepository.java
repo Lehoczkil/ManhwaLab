c7acdc67574b425384e1ffd45d6df0c2bfa42206
AronBodzas <aron.bodzas42@gmail.com>
2023-01-03 10:26:27 +0100

    Feat: get genres from db

--- backend/src/main/java/com/codecool/manhwalabbackend/repository/GenreRepository.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/repository/GenreRepository.java
@@ -2,10 +2,18 @@ package com.codecool.manhwalabbackend.repository;
 
 import com.codecool.manhwalabbackend.model.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    @Query(value = "SELECT genre.name FROM genre " +
+            "JOIN manhwa_profile_genre_list mpgl on genre.id = mpgl.genre_list_id " +
+            "WHERE manhwa_profile_id = ?1", nativeQuery = true)
+    List<String> getManhwaGenres(int manhwaId);
 
 }
 
