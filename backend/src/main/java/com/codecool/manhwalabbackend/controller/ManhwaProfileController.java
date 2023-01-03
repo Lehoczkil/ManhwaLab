133830b886ed3e1a4fd1c3c40400c30e5fbf604a
AronBodzas <aron.bodzas42@gmail.com>
2023-01-03 10:27:58 +0100

    Refactor

--- backend/src/main/java/com/codecool/manhwalabbackend/controller/ManhwaProfileController.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/controller/ManhwaProfileController.java
@@ -1,6 +1,7 @@
 package com.codecool.manhwalabbackend.controller;
 
 import com.codecool.manhwalabbackend.model.ManhwaProfile;
+import com.codecool.manhwalabbackend.service.GenreService;
 import com.codecool.manhwalabbackend.service.ManhwaProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +16,12 @@ import java.util.List;
 public class ManhwaProfileController {
 
     ManhwaProfileService manhwaProfileService;
+    GenreService genreService;
 
     @Autowired
-    public ManhwaProfileController(ManhwaProfileService manhwaProfileService) {
+    public ManhwaProfileController(ManhwaProfileService manhwaProfileService, GenreService genreService) {
         this.manhwaProfileService = manhwaProfileService;
+        this.genreService = genreService;
     }
 
     @GetMapping(value = "/{manhwaName}")
@@ -35,7 +38,7 @@ public class ManhwaProfileController {
     @GetMapping(value = "/{manhwaName}/genres")
     public List<String> getManhwaGenres(@PathVariable String manhwaName){
         manhwaName = "Murim Login";
-        return manhwaProfileService.getManhwaGenres(manhwaProfileService.getManhwaProfileByName(manhwaName).getId().intValue());
+        return genreService.getManhwaGenres(manhwaProfileService.getManhwaProfileByName(manhwaName).getId().intValue());
     }
 
     @GetMapping(value = "/{manhwaName}/themes")
--- backend/src/main/java/com/codecool/manhwalabbackend/repository/ManhwaProfileRepository.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/repository/ManhwaProfileRepository.java
@@ -16,10 +16,6 @@ public interface ManhwaProfileRepository extends JpaRepository<ManhwaProfile, Lo
             "WHERE manhwa_profile.name LIKE ?1", nativeQuery = true)
     Float getManhwaProfileRatingByName(String name);
 
-    @Query(value = "SELECT genre.name FROM genre " +
-            "JOIN manhwa_profile_genre_list mpgl on genre.id = mpgl.genre_list_id " +
-            "WHERE manhwa_profile_id = ?1", nativeQuery = true)
-    List<String> getManhwaGenres(int manhwaId);
 
 }
 
