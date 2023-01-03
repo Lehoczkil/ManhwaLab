c56efef1599f244769ca37f9f09d7c19e81fba6f
AronBodzas <aron.bodzas42@gmail.com>
2023-01-03 12:26:05 +0100

    refactor: typo

--- backend/src/main/java/com/codecool/manhwalabbackend/repository/ThemeRepository.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/repository/ThemeRepository.java
@@ -13,7 +13,7 @@ public interface ThemeRepository extends JpaRepository<Theme, Long> {
     @Query(value = "SELECT theme.name FROM theme " +
             "JOIN manhwa_profile_theme_list mptl on theme.id = mptl.theme_list_id " +
             "WHERE manhwa_profile_id = ?1", nativeQuery = true)
-    List<String> getManhwaThemes(int manwaId);
+    List<String> getManhwaThemes(int manhwaId);
 
 }
 
