13dfa4dc9e7c284a2d0dda2a030e66a6d2170f82
AronBodzas <aron.bodzas42@gmail.com>
2023-01-03 11:27:31 +0100

    refactor: rename base url

--- backend/src/main/java/com/codecool/manhwalabbackend/controller/ManhwaProfileController.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/controller/ManhwaProfileController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/manhwa")
+@RequestMapping("/api/manhwaLab")
 public class ManhwaProfileController {
 
     ManhwaProfileService manhwaProfileService;
