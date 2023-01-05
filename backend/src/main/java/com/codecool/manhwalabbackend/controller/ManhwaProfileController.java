790bfad87e99761bdc347ef024259593c0fc3bb7
AronBodzas <aron.bodzas42@gmail.com>
2023-01-05 10:50:19 +0100

    fix: added curly brackets to @CrossOrigin

--- backend/src/main/java/com/codecool/manhwalabbackend/controller/ManhwaProfileController.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/controller/ManhwaProfileController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:8081")
+@CrossOrigin({"http://localhost:8081"})
 @RequestMapping("/api/manhwaLab")
 public class ManhwaProfileController {
 
