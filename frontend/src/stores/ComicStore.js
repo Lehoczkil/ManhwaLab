07f235d66f628e1ef54d528f75290254be954ce7
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-01-10 10:36:29 +0100

    feat: Filter comics by genre

--- frontend/src/stores/ComicStore.js
+++ frontend/src/stores/ComicStore.js
@@ -2,11 +2,10 @@ import { defineStore } from "pinia";
 
 export const useComicStore = defineStore("comicStore", {
   state: () => ({
-    currentComic: {},
+    originals: [],
     comics: [],
     loading: false,
   }),
-  getters: {},
   actions: {
     async getComics() {
       this.loading = true;
@@ -14,12 +13,31 @@ export const useComicStore = defineStore("comicStore", {
       const data = await res.json();
 
       this.comics = data;
+      this.originals = data;
       this.loading = false;
     },
     async getComicByName(name) {
       const res = await fetch("http://localhost:8080/api/manhwaLab/" + name);
       const data = await res.json();
       this.currentComic = data;
+    },
+    filterComics(genre, theme, type) {
+      this.comics = this.originals;
+      let filteredComics = this.originals;
+      if (genre) {
+        filteredComics = filteredComics.filter((comic) =>
+          comic.genreList.map((g) => g.name).includes(genre)
+        );
+      }
+      if (theme) {
+        filteredComics = filteredComics.filter(
+          (comic) => comic.theme === theme
+        );
+      }
+      if (type) {
+        filteredComics = filteredComics.filter((comic) => comic.type === type);
+      }
+      this.comics = filteredComics;
     },
   },
 });
