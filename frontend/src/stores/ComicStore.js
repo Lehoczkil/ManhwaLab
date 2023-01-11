789aaf7d1aef043a962258fa152810a59e2fba0b
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-01-11 10:12:31 +0100

    feat: Implement sorting comics in store

--- frontend/src/stores/ComicStore.js
+++ frontend/src/stores/ComicStore.js
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 export const useComicStore = defineStore("comicStore", {
   state: () => ({
     originals: [],
+    currentComic: {},
     comics: [],
     loading: false,
   }),
@@ -21,7 +22,31 @@ export const useComicStore = defineStore("comicStore", {
       const data = await res.json();
       this.currentComic = data;
     },
-    filterComics(genre, theme, type) {
+    sortComics(sort) {
+      let sortedComics = this.comics;
+      if (sort === "view") {
+        sortedComics.sort(function (a, b) {
+          return b.views - a.views;
+        });
+      } else if (sort === "rating") {
+        sortedComics.sort(function (a, b) {
+          return a.rating - b.rating;
+        });
+      } else if (sort === "abc") {
+        sortedComics.sort(function (a, b) {
+          if (a.title < b.title) {
+            return -1;
+          }
+          if (a.title > b.title) {
+            return 1;
+          }
+          return 0;
+        });
+
+        this.comics = sortedComics;
+      }
+    },
+    filterComics(genre, theme, type, sort) {
       this.comics = this.originals;
       let filteredComics = this.originals;
       if (genre) {
@@ -37,7 +62,9 @@ export const useComicStore = defineStore("comicStore", {
       if (type) {
         filteredComics = filteredComics.filter((comic) => comic.type === type);
       }
+
       this.comics = filteredComics;
+      this.sortComics(sort);
     },
   },
 });
