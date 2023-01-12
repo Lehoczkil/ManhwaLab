323aa4827fb945ccd7efac95dac81edb2276d72a
AronBodzas <aron.bodzas42@gmail.com>
2023-01-12 11:44:46 +0100

    Feat: getTopFiveMonthlyComic method returns top five comic for the month

--- backend/src/main/java/com/codecool/manhwalabbackend/service/popularity/PopularityPerMonthService.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/popularity/PopularityPerMonthService.java
@@ -1,14 +1,23 @@
 package com.codecool.manhwalabbackend.service.popularity;
 
+import com.codecool.manhwalabbackend.model.DTO.ManhwaProfileDTO;
+import com.codecool.manhwalabbackend.model.ManhwaProfile;
 import com.codecool.manhwalabbackend.model.date.StartDateOfViewCounting;
+import com.codecool.manhwalabbackend.model.popularity.ComicPopularityPerMonth;
+import com.codecool.manhwalabbackend.repository.ManhwaProfileRepository;
 import com.codecool.manhwalabbackend.repository.date.StartDateOfViewCountsRepository;
 import com.codecool.manhwalabbackend.repository.popularity.PopularityPerMonthRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +25,8 @@ public class PopularityPerMonthService {
 
     private final PopularityPerMonthRepository popularityPerMonthRepository;
     private final StartDateOfViewCountsRepository startDateOfViewCountsRepository;
+    private final ManhwaProfileRepository manhwaProfileRepository;
+
 
 
     public void nullifyMonthlyViews(Long id){
@@ -31,5 +42,20 @@ public class PopularityPerMonthService {
         if(durationFromStartDate.compareTo(durationOfOneDay) > equalTime) {
             popularityPerMonthRepository.nullifyMonthlyViews(id);
         }
+    }
+
+    public List<ManhwaProfileDTO> getTopFiveMonthlyComic(){
+        Sort sort = Sort.by(Sort.Direction.DESC, "viewsThisMonth");
+        Pageable pageable = PageRequest.of(0, 5, sort);
+        List<ManhwaProfileDTO> topFiveManhwaToday = new ArrayList<>();
+        for (ComicPopularityPerMonth comicPopularityPerMonth : popularityPerMonthRepository.findAll(pageable).getContent()) {
+
+            Long comicId = comicPopularityPerMonth.getComicId();
+            ManhwaProfile manhwaProfileById = manhwaProfileRepository.getManhwaProfileById(comicId);
+            String title = manhwaProfileById.getTitle();
+            topFiveManhwaToday.add(new ManhwaProfileDTO(comicId, title));
+        }
+        return topFiveManhwaToday;
+
     }
 }
