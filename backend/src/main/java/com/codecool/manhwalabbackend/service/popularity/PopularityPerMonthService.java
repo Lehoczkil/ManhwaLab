dd7cec9758bcb635cce845529a54f0ed10ef565d
AronBodzas <aron.bodzas42@gmail.com>
2023-01-12 09:38:27 +0100

    feat: nullifying monthly views when one month passed

--- backend/src/main/java/com/codecool/manhwalabbackend/service/popularity/PopularityPerMonthService.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/popularity/PopularityPerMonthService.java
@@ -1,16 +1,36 @@
 package com.codecool.manhwalabbackend.service.popularity;
 
+import com.codecool.manhwalabbackend.model.date.StartDateOfViewCounts;
+import com.codecool.manhwalabbackend.repository.date.StartDateOfViewCountsRepository;
 import com.codecool.manhwalabbackend.repository.popularity.PopularityPerMonthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Year;
 
 @Service
 @RequiredArgsConstructor
 public class PopularityPerMonthService {
 
-    private PopularityPerMonthRepository popularityPerMonthRepository;
+    private final PopularityPerMonthRepository popularityPerMonthRepository;
+    private final StartDateOfViewCountsRepository startDateOfViewCountsRepository;
 
-    public void refreshDataBase(Long id){
-        popularityPerMonthRepository.nullifyData(id);
+
+    public void nullifyMonthlyViews(Long id){
+        Long idOfStartTimes = 1L;
+        StartDateOfViewCounts data = startDateOfViewCountsRepository.getStartDateOfViewCountsById(idOfStartTimes);
+        LocalDateTime startDate = data.getStartDateOfMonthlyViews();
+        LocalDateTime currentDate = LocalDateTime.now();
+        Month currentMonth = data.getCurrentMonth();
+        int daysInMonth = currentMonth.length(false);
+        Duration durationOfOneDay = Duration.ofDays(daysInMonth);
+        Duration durationFromStartDate = Duration.between(startDate, currentDate);
+        int equalTime = 0;
+        if(durationFromStartDate.compareTo(durationOfOneDay) > equalTime) {
+            popularityPerMonthRepository.nullifyMonthlyViews(id);
+        }
     }
 }
