cfe98ca239b895b42797af9e1133e3a574656df3
AronBodzas <aron.bodzas42@gmail.com>
2023-01-12 09:37:28 +0100

    feat: nullifying daily views when one day passed

--- backend/src/main/java/com/codecool/manhwalabbackend/service/popularity/PopularityPerDayService.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/popularity/PopularityPerDayService.java
@@ -1,17 +1,31 @@
 package com.codecool.manhwalabbackend.service.popularity;
 
+import com.codecool.manhwalabbackend.repository.date.StartDateOfViewCountsRepository;
 import com.codecool.manhwalabbackend.repository.popularity.PopularityPerDayRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class PopularityPerDayService {
 
-    private PopularityPerDayRepository popularityPerDayRepository;
+    private final PopularityPerDayRepository popularityPerDayRepository;
+    private final StartDateOfViewCountsRepository startDateOfViewCountsRepository;
 
-    public void refreshDataBase(Long id){
-        popularityPerDayRepository.nullifyData(id);
+    public void nullifyDailyViews(Long id){
+        Long idOfStartTimes = 1L;
+        LocalDateTime startDate = startDateOfViewCountsRepository.getStartDateOfViewCountsById(idOfStartTimes).getStartDateOfDailyViews();
+        LocalDateTime currentDate = LocalDateTime.now();
+        int oneDay = 1;
+        Duration durationOfOneDay = Duration.ofDays(oneDay);
+        Duration durationFromStartDate = Duration.between(startDate, currentDate);
+        int equalTime = 0;
+        if(durationFromStartDate.compareTo(durationOfOneDay) > equalTime) {
+            popularityPerDayRepository.nullifyDailyViews(id);
+        }
     }
 }
