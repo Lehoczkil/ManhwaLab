68ea34c95a2e9096addf25eb8a88a66eced896a5
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-01-23 11:59:44 +0100

    style: Make title centered in small screens in top list

--- frontend/src/components/TopList.vue
+++ frontend/src/components/TopList.vue
@@ -1,11 +1,21 @@
 <template>
     <section>
-        <h1>{{ title }}</h1>
+        <h1 class="title">{{ title }}</h1>
         <div class="shows">
             <TopComic v-for="comic in comics" :key="comic.id" :comic="comic" />
         </div>
     </section>
 </template>
+
+<style scoped>
+@media (max-width:950px) {
+    .title {
+        text-align: center;
+        font-size: 2rem;
+        margin-block: 3vh;
+    }
+}
+</style>
 
 <script>
 import TopComic from './TopComic'
