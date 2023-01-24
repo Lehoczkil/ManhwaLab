6d307cccc039c61cf1b58b49505cd8f979b14d09
AronBodzas <aron.bodzas42@gmail.com>
2023-01-24 11:43:05 +0100

    Refactor: removing unnecessary commands

--- backend/build-docker.sh
+++ backend/build-docker.sh
@@ -1,7 +1,4 @@
-export DB_USERNAME="aron"
-export DB_PASSWORD="Sisakvirag17"
 
-mvn clean package
 docker build -t manhwalab-app:manhwalab -f backend.dockerfile .
 docker build -t manhwalab-database:manhwalab-db -f manhwaLabDatabase.dockerfile .
 sudo ufw allow 3000
