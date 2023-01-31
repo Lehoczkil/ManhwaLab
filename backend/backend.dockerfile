f8337b2a4ca3778311c861df417ecfb6f4b3dbdf
AronBodzas <aron.bodzas42@gmail.com>
2023-01-31 11:22:48 +0100

    refactor: Removing unused files for deployment

--- backend/backend.dockerfile
+++
@@ -1,7 +0,0 @@
-FROM alpine
-MAINTAINER username
-RUN apk --update add openjdk11-jre
-COPY target/ManhwaLabBackend-0.0.1-SNAPSHOT.jar backend.jar
-CMD ["java", "-jar", "backend.jar"]
-
-EXPOSE 8080
--- backend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-export DB_USERNAME=""
-export SPRING_DATASOURCE_URL=""
-export DB_PASSWORD=""
-
-mvn clean package
-java -jar target/ManhwaLabBackend-0.0.1-SNAPSHOT.jar backend.jar
