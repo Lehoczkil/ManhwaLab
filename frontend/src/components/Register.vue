da9a71aafdb403d8a4bb808836135304bceb2fbc
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-02-01 12:22:16 +0100

    feat: Add modal for registering new user

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="modal-backdrop">
+        <form class="modal">
+            <header class="modal-header">
+                <h2>Register</h2>
+                <button type="button" class="btn-close" @click="close">
+                    x
+                </button>
+            </header>
+
+            <section class="modal-body">
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
+                <input type="email" placeholder="Email">
+            </section>
+
+            <footer class="modal-footer">
+                <button type="submit" class="btn login">
+                    Register
+                </button>
+            </footer>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+    .modal-body {
+        height: 45%;
+    }
+</style>
+
+
+<script>
+export default {
+    name: 'Register',
+    methods: {
+        close() {
+            this.$emit('close');
+        },
+    },
+};
+</script>
