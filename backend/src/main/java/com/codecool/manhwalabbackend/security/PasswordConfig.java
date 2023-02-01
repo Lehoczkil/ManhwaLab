10bc74e5304e303ee28f898e456e690ef32ac21c
AronBodzas <aron.bodzas42@gmail.com>
2023-02-01 11:33:42 +0100

    feat: Creating passwordConfig class to encode password

---
+++ backend/src/main/java/com/codecool/manhwalabbackend/security/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.codecool.manhwalabbackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    //TODO : encode the password
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();}
+}
