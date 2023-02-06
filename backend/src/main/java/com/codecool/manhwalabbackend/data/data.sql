bf67605213520fc11238076cffb4bd79cd7535b1
AronBodzas <aron.bodzas42@gmail.com>
2023-02-06 10:08:46 +0100

    refactor: different image sizes

--- backend/src/main/java/com/codecool/manhwalabbackend/data/data.sql
+++ backend/src/main/java/com/codecool/manhwalabbackend/data/data.sql
@@ -33,19 +33,19 @@ INSERT INTO theme (name) VALUES ('Dungeons');--10
 
 ---------------------------------------------------------------------------------------------
 -- COMIC PROFILE
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('Zerobic', 'An era where hunters thrive on hunting monsters that come from Gates. Jin Tae-Kyung is a low-rank hunter who picks up a VR machine, and accidentally logs into the game, which is set in the world of Martial Arts. After many ups and downs, Tae-Kyung is able to escape this world. The strength and skills, which he received in the Murim were able to be carried over back to the real world. This allows him to continue making a living a hunter… but he decides to return back to the world of Murim, out of care for his NPC friends.'
-       , 4283, 'Murim Login', 135, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 0, 9.3, '/images/murim-login.webp');
+       , 4283, 'Murim Login', 135, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 0, 9.3, '/images/murim-login-big.webp', '/images/murim-login-small.webp');
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('Hermode/Cho Wooneh (RedIce Studio)', 'The mobile gacha game ‘Pick Me Up!’ is known for being brutally difficult, and no one has been able to clear a dungeon. Loki, the fifth among all the world’s masters, loses consciousness while trying to clear the dungeon. Upon waking up, Loki finds himself turned into a 1-star level 1 hero, ‘Islat Han’. “It’s him! I’m certain he’s the one who brought me here!” In order to return to Earth, he must lead novice masters and heroes and break through the 100th floor of the dungeon! “You messed with the wrong person.” This is the story of master Loki who is forced to carry everyone to victory and cannot afford a single defeat.'
-       , 456, 'Pick Me Up, Infinite Gacha', 20, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 23423, 10.0, '/images/pick-me-up-infinite-gacha.webp');
+       , 456, 'Pick Me Up, Infinite Gacha', 20, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 23423, 10.0, '/images/pick-me-up-infinite-gacha-big.webp', '/images/pick-me-up-infinite-gacha-small.webp');
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('-', 'If you max out your level in-game, you can get heaven-defying powers in real life?! Player Ling Ce finally obtained this reward through his hard work but ended up receiving the most useless skills. Just as he was mourning his luck, a mysterious power brought him back to three years in the past. With the experiences and memories of his previous life, how many different wonderful things can he bring to his new life?'
-       , 1450, 'The Game That I Came From', 149, CURRENT_DATE, 'ONGOING', 'MANHUA', CURRENT_DATE, 39824, 6.6, '/images/the-game-that-i-came-from.webp');
+       , 1450, 'The Game That I Came From', 149, CURRENT_DATE, 'ONGOING', 'MANHUA', CURRENT_DATE, 39824, 6.6, '/images/the-game-that-i-came-from-big.webp', '/images/the-game-that-i-came-from-small.webp');
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('-', 'Jin Runcandel was the youngest son of Runcandel, the land’s most prestigious swordsman family…
 And the biggest failure in Runcandel history.
 
@@ -56,18 +56,18 @@ He, who was kicked out miserably and came to a meaningless end, was given anothe
 
 Memories of his past life, overwhelming talent, and a contract with God…
 The preparations to become the greatest are complete.'
-       , 340, 'Swordmaster’s Youngest Son', 53, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 3361, 7.9, '/images/swordmasters-youngest-son.webp');
+       , 340, 'Swordmaster’s Youngest Son', 53, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 3361, 7.9, '/images/swordmasters-youngest-son-big.webp', '/images/swordmasters-youngest-son-small.webp');
 
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('-', 'After being accused of murdering a colleague, Cassian Lee came to his death. The moment he thought it was all over, his soul was suddenly possessed by a boy’s body and he awakens as a villain'
-       , 14890, 'Villain To Kill', 79, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 66798, 6.9, '/images/villain-to-kill.webp');
+       , 14890, 'Villain To Kill', 79, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 66798, 6.9, '/images/villain-to-kill-big.webp', '/images/villain-to-kill-small.webp');
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('-', 'Cheon Haesun is the protagonist who‘s terminally ill because of various poisons. Cheon Haesun, who was never welcome anywhere, will have his story of becoming the strongest hunter play out on an Earth upon which dungeons and monsters have broken out!'
-       , 1912, 'Poison-Eating Healer', 23, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 666, 9.6, '/images/poison-eating-healer.webp');
+       , 1912, 'Poison-Eating Healer', 23, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 666, 9.6, '/images/poison-eating-healer-big.webp', '/images/poison-eating-healer-small.webp');
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('Eunyeol', 'If the Grim Reaper calls your name three times, you will be taken to the underworld.
 [Hyeokryeon Mugang.]
 [Hyeokryeon Mugang.]
@@ -79,18 +79,18 @@ After desperately eating the herb of immortality, I couldn’t believe it, but I
 When I realized this was real and opened my eyes, there were old geezers standing around me…!
 Where am I…? Mount Wudang? The Wudang sect?! And what’s even worse, I’ve possessed the body of an errand boy. …This is nuts!!!
 I… I am… the strongest… the absolute f**king strongest Heavenly Leader of the Evil sect, Hyeokryeon Mugang!'
-       , 10265, 'Wandering Warrior of Wudang', 84, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 78924, 2.9, '/images/wandering-warrior-of-wudang.webp');
+       , 10265, 'Wandering Warrior of Wudang', 84, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 78924, 2.9, '/images/wandering-warrior-of-wudang-big.webp', '/images/wandering-warrior-of-wudang-small.webp');
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('Kim Pyeongbeom', 'The greatest martial family, Samion. Daven, a member of the Samion family, is a one-armed martial soldier without a right arm. Despite the ridicule and contempt he got for only having a left arm, he surpassed the direct line of the Samion family like a genius. However, he got betrayed by his family and parents in the end, thus meeting a poor end. But, he was reincarnated. “I have a right arm?” He had a new family tradition. A naturally gifted body. Experience from his past life. And the god Yulion with him as well. After his reincarnation, everything changed, and a new adventure started.
 '
-       , 1295, 'Standard of Reincarnation', 41, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 86234, 5.5, '/images/standard-of-reincarnation.webp');
+       , 1295, 'Standard of Reincarnation', 41, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 86234, 5.5, '/images/standard-of-reincarnation-big.webp', '/images/standard-of-reincarnation-small.webp');
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('-', 'Hamel, a warrior who traveled with his colleagues to exterminate the devil. Unfortunately, he died just before the battle with the devil started. “Yes.” What the hell? He was reborn as the descendant of his fellow warrior, Vermouth. The descendant from the bloodline of the great Vermouth, Hamel. No, he was now Eugene Lionhart. “It was already enough with me having the delusion that I was a genius in my past life. But… this… It’s… It’s incomparable.’ From a dying body to a new body, he has the achievement that he did not have in his previous life. And, a reincarnation of an unknown origin, the reality of living together with the demons. Facing a new world where everything is questionable. The journey of the unfinished previous life begins with Eugene’s body.'
-       , 4521, 'Damn Reincarnation', 47, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 74823, 4.6, '/images/damn-reincarnation.webp');
+       , 4521, 'Damn Reincarnation', 47, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 74823, 4.6, '/images/damn-reincarnation-big.webp', '/images/damn-reincarnation-small.webp');
 
-INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page)
+INSERT INTO comic_profile (author, description, favourite, title, number_of_chapters, released, status, type, updated, views, rating, cover_page_big, cover_page_small)
 VALUES ('-', 'An Earth in the 21st century that has fused with another world. An era of “dungeons” teeming with monsters and the “adventurers” that raid them.
 
 Also, the most elite educational facility in South Korea that fosters these adventurers: “Warrior High School.”
@@ -100,7 +100,7 @@ Although he was once considered the greatest adventurers in Asia, he has been ac
 “You there, get in Warrior High School.”
 
 This is a story about a young man who’s lost his dream, hiding his identity, finding himself once again.'
-       , 4056, 'Warrior High School – Dungeon Raid Department', 27, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 274658123, 8.6, '/images/warrior-high-school-dungeon-raid-department.webp');
+       , 4056, 'Warrior High School – Dungeon Raid Department', 27, CURRENT_DATE, 'ONGOING', 'MANHWA', CURRENT_DATE, 274658123, 8.6, '/images/warrior-high-school-dungeon-raid-department-big.webp', '/images/warrior-high-school-dungeon-raid-department-small.webp');
 
 
 ---------------------------------------------------------------------------------------------
@@ -198,7 +198,7 @@ INSERT INTO theme_for_comic ( comic_profile_id, theme_id) VALUES ( 10, 10);
 ---------------------------------------------------------------------------------------------
 -- USER PROFILE
 
-INSERT INTO user_profile (password, age, description, gender, joined, last_online, location, user_name) VALUES ('pw', 42, '', 'MALE', DATE_TRUNC('day', CURRENT_DATE ), DATE_TRUNC('day', CURRENT_DATE ), 'Budapest', 'Béla');
+INSERT INTO user_profile (password, age, description, gender, joined, last_online, location, username) VALUES ('pw', 42, '', 'MALE', DATE_TRUNC('day', CURRENT_DATE ), DATE_TRUNC('day', CURRENT_DATE ), 'Budapest', 'Béla');
 
 ---------------------------------------------------------------------------------------------
 -- COMMENT
