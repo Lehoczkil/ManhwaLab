70d293c6aa760c4788aa85c8997a4ed449c0d61a
AronBodzas <aron.bodzas42@gmail.com>
2023-02-09 11:24:43 +0100

    feat: Creating /profile endpoint

--- backend/src/main/java/com/codecool/manhwalabbackend/controller/UserProfileController.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/controller/UserProfileController.java
@@ -1,10 +1,12 @@
 package com.codecool.manhwalabbackend.controller;
 
+import com.codecool.manhwalabbackend.model.UserProfile;
 import com.codecool.manhwalabbackend.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("api/manhwaLab")
@@ -13,4 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserProfileController {
 
     private final UserProfileService userProfileService;
+
+    @GetMapping(value = "/profile")
+    public UserProfile getUser(){
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userProfileService.getUser(username);
+    }
+
+
+
+
+
+
 }
