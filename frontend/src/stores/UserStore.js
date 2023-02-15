b0a643c8832319de545be13efdaa7761fcf76d6b
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-02-15 10:51:52 +0100

    feat: Fetch users data and store it in states in store

--- frontend/src/stores/UserStore.js
+++ frontend/src/stores/UserStore.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("userStore", {
   state: () => ({
-    name: "",
+    username: "",
     age: "",
     gender: "",
     location: "",
@@ -11,16 +11,30 @@ export const useUserStore = defineStore("userStore", {
     description: "",
     finished: [],
     read: [],
-    favorites: [],
+    favourites: [],
     readLater: [],
   }),
   actions: {
     async getUser() {
-      await fetch("/api/manhwaLab/profile", {
+      const response = await fetch("/api/manhwaLab/profile", {
         headers: {
-          "Authorization": JSON.parse(localStorage.getItem("tokenStore")).token,
+          Authorization: JSON.parse(localStorage.getItem("tokenStore")).token,
         },
       });
+
+      const user = await response.json();
+
+      this.username = user.username !== null ? user.username : "Not known" ;
+      this.age = user.age !== null ? user.age : "Not known" ;
+      this.gender = user.gender !== null ? user.gender : "Not known" ;
+      this.location = user.location !== null ? user.location : "Not known" ;
+      this.lastOnline = user.lastOnline !== null ? user.lastOnline : "Not known" ;
+      this.joined = user.joined !== null ? user.joined : "Not known" ;
+      this.description = user.description !== null ? user.description : "There is no description" ;
+      this.finished = user.finished !== null ? user.finished: "Nothing here" ;
+      this.read = user.read !== null ? user.read: "Nothing here" ;
+      this.favourites = user.favourites !== null ? user.favourites: "Nothing here" ;
+      this.readLater = user.readLater !== null ? user.readLater: "Nothing here" ;
     },
   },
 });
