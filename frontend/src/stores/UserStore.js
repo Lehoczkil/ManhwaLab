553bf1ced67ced63b4ea9e3138600a8397f8faf8
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-02-16 11:23:20 +0100

    feat: Implement updating user profile in user store

--- frontend/src/stores/UserStore.js
+++ frontend/src/stores/UserStore.js
@@ -24,17 +24,58 @@ export const useUserStore = defineStore("userStore", {
 
       const user = await response.json();
 
-      this.username = user.username !== null ? user.username : "Not known" ;
-      this.age = user.age !== null ? user.age : "Not known" ;
-      this.gender = user.gender !== null ? user.gender : "Not known" ;
-      this.location = user.location !== null ? user.location : "Not known" ;
-      this.lastOnline = user.lastOnline !== null ? user.lastOnline : "Not known" ;
-      this.joined = user.joined !== null ? user.joined : "Not known" ;
-      this.description = user.description !== null ? user.description : "There is no description" ;
-      this.finished = user.finished !== null ? user.finished: "Nothing here" ;
-      this.read = user.read !== null ? user.read: "Nothing here" ;
-      this.favourites = user.favourites !== null ? user.favourites: "Nothing here" ;
-      this.readLater = user.readLater !== null ? user.readLater: "Nothing here" ;
+      this.username = user.username !== null ? user.username : "Not known";
+      this.age = user.age !== null ? user.age : "Not known";
+      this.gender = user.gender !== null ? user.gender : "Not known";
+      this.location = user.location !== null ? user.location : "Not known";
+      this.lastOnline =
+        user.lastOnline !== null ? user.lastOnline : "Not known";
+      this.joined = user.joined !== null ? user.joined : "Not known";
+      this.description =
+        user.description !== null
+          ? user.description
+          : "There is no description";
+      this.finished = user.finished !== null ? user.finished : "Nothing here";
+      this.read = user.read !== null ? user.read : "Nothing here";
+      this.favourites =
+        user.favourites !== null ? user.favourites : "Nothing here";
+      this.readLater =
+        user.readLater !== null ? user.readLater : "Nothing here";
+    },
+    async updateUser(
+      username,
+      age,
+      gender,
+      location,
+      lastOnline,
+      joined,
+      description
+    ) {
+      await fetch("/api/manhwaLab/updateUser", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: JSON.parse(localStorage.getItem("tokenStore")).token,
+        },
+        body: JSON.stringify({
+          username: username,
+          age: age,
+          gender: gender,
+          location: location,
+          lastOnline: lastOnline,
+          joined: joined,
+          description: description,
+        }),
+      });
+
+      this.username = username !== null ? username : "Not known";
+      this.age = age !== null ? age : "Not known";
+      this.gender = gender !== null ? gender : "Not known";
+      this.location = location !== null ? location : "Not known";
+      this.lastOnline = lastOnline !== null ? lastOnline : "Not known";
+      this.joined = joined !== null ? joined : "Not known";
+      this.description =
+        description !== null ? description : "There is no description";
     },
   },
 });
