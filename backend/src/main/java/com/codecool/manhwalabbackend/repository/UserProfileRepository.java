c5b5c358a4d6e9e8df2abe76ad6062bf896ec588
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-02-21 10:59:02 +0100

    feat: Add methods to get emails and usernames of users

--- backend/src/main/java/com/codecool/manhwalabbackend/repository/UserProfileRepository.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/repository/UserProfileRepository.java
@@ -2,12 +2,20 @@ package com.codecool.manhwalabbackend.repository;
 
 import com.codecool.manhwalabbackend.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
     UserProfile getUserProfileByUsername(String username);
 
+    @Query(value = "SELECT email FROM user_profile", nativeQuery = true)
+    ArrayList<String> getUserEmails();
+
+    @Query(value = "SELECT username FROM user_profile", nativeQuery = true)
+    ArrayList<String> getUsernames();
 }
 
