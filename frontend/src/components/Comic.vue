695f359db3b65acebec3f2d75e9de297ba403552
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-02-24 10:36:28 +0100

    feat: Implement removing comics from users lists in comic

--- frontend/src/components/Comic.vue
+++ frontend/src/components/Comic.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="comic" @click="handleClick">
+        <button v-if="fromShortList" class="remove" @click.stop="handleRemove">-</button>
         <img :src="`${comic.coverPageSmall}`" alt="picture about the comic" class="pic">
         <h1>{{ comic.title }}</h1>
         <p>{{ comic.numberOfChapters }} chapters</p>
@@ -25,6 +26,7 @@
 }
 
 .comic {
+    position: relative;
     color: white;
     flex-basis: 18%;
     height: clamp(430px, 26vh, 3200px);
@@ -54,11 +56,33 @@ span {
     border-radius: var(--radius);
     border: 0.5px solid white;
 }
+
+.remove {
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 3rem;
+    height: 2rem;
+    color: white;
+    background: transparent;
+    border: none;
+    font-size: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.remove:hover,
+.remove:focus {
+    color: var(--blue);
+}
 </style>
 
 <script>
 import { useTokenStore } from '@/stores/TokenStore'
+import { useUserStore } from '@/stores/UserStore'
 import { storeToRefs } from 'pinia'
+import { getCurrentInstance } from 'vue';
 
 export default {
     name: "Comic",
@@ -74,6 +98,12 @@ export default {
             default() {
                 return false
             }
+        },
+        category: {
+            type: String,
+            default() {
+                return ''
+            }
         }
     },
     methods: {
@@ -83,12 +113,35 @@ export default {
             } else {
                 this.$router.push(`/comics/${this.comic.comicId}`)
             }
+        },
+        handleRemove() {
+            if (this.category !== '') {
+                switch (this.category) {
+                    case 'Reading':
+                        this.userStore.removeFromReading(this.comic)
+                        break
+                    case 'Read Later':
+                        this.userStore.removeFromReadLater(this.comic)
+                        break
+                    case 'Finished':
+                        this.userStore.removeFromFinished(this.comic)
+                        break
+                    case 'Favourites':
+                        this.userStore.removeFromFavourites(this.comic)
+                        break
+                }
+                const instance = getCurrentInstance();
+                instance.proxy.$forceUpdate();
+            }
         }
     },
     setup() {
         const tokenStore = useTokenStore()
+        const userStore = useUserStore()
+
         const { token } = storeToRefs(tokenStore)
-        return { tokenStore, token }
+
+        return { userStore, tokenStore, token }
     }
 }
 </script>
