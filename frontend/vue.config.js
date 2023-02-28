7abc2581963a39f1f9f7a8ee575e5d30227bcf08
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-02-28 11:12:19 +0100

    chore: Update proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/': {
-          target: 'http://localhost:8080',
+          target: 'http://ec2-3-75-0-59.eu-central-1.compute.amazonaws.com:8080',
           ws: false,
           changeOrigin: true,
         },
