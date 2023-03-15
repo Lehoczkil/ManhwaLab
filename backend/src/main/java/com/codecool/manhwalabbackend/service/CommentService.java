6ef8104cfc64b7a6e56fea4b7c9f24cd8d484a26
AronBodzas <aron.bodzas42@gmail.com>
2023-03-15 13:27:05 +0100

    feat: Create getComments method, addNewComment method and the newCommentBuilder utility method

--- backend/src/main/java/com/codecool/manhwalabbackend/service/CommentService.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/service/CommentService.java
@@ -1,14 +1,37 @@
 package com.codecool.manhwalabbackend.service;
 
+import com.codecool.manhwalabbackend.model.Comment;
+import com.codecool.manhwalabbackend.model.enums.CommentType;
 import com.codecool.manhwalabbackend.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final ComicProfileService comicProfileService;
+    private final UserProfileService userProfileService;
+
+    public List<Comment> getComicComments(Long comicId){
+     return commentRepository.getCommentsByParentComicId(comicId);
+    }
+
+    public void addNewComment(Long comicId, String comment, String username){
+        commentRepository.save(newCommentBuilder(comicId, comment, username));
+    }
+
+    private Comment newCommentBuilder(Long comicId, String commentText, String username){
+        return new Comment(
+                comicId, commentText, userProfileService.getUser(username),
+                null, CommentType.COMMENT, null,
+                0, 0, comicProfileService.getComicProfileById(comicId));
+    }
 
 }
 
