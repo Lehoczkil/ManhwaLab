b981749082dc1c5e341612ced20185531328c375
AronBodzas <aron.bodzas42@gmail.com>
2023-03-15 13:26:14 +0100

    feat: Create getCommentsByParentComicId method

--- backend/src/main/java/com/codecool/manhwalabbackend/repository/CommentRepository.java
+++ backend/src/main/java/com/codecool/manhwalabbackend/repository/CommentRepository.java
@@ -4,7 +4,13 @@ import com.codecool.manhwalabbackend.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+
+    List<Comment> getCommentsByParentComicId(Long comicId);
+
 
 }
