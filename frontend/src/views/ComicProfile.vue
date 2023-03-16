bce7445835bb0c4152e1a95ea9565e3c15b2cf5e
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-03-16 09:48:32 +0100

    fix: Displaying image in comic profile

--- frontend/src/views/ComicProfile.vue
+++ frontend/src/views/ComicProfile.vue
@@ -3,7 +3,7 @@
         <main>
             <div class="pic-container">
                 <div class="img-container">
-                    <img :src="`${currentComic.coverPageBig}`" alt="Cover for the comic" class="pic">
+                    <img :src="`${baseUrl}${currentComic.coverPageBig}`" alt="Cover for the comic" class="pic">
                     <div class="btn-row">
                         <button v-if="tokenStore.isTokenExists()" class="add" @click="handleAdd('Reading')">Add to Reading</button>
                         <button v-if="tokenStore.isTokenExists()" class="add" @click="handleAdd('Later')">Add to Read Later</button>
@@ -430,6 +430,11 @@ export default {
     },
     props: {
         id: String
+    },
+    computed: {
+        baseUrl() {
+            return process.env.VUE_APP_API_BASE
+        }
     },
     methods: {
         handleFilterByGenre(genre) {
